e3fd773027e79d663b04111a98347e6e171cc71e
ADHIDHYA R S <rsadithya76@gmail.com>
2026-08-20 21:05:41 +0530

    Time: 5 ms (82.93%), Space: 77.4 MB (29.76%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -5,9 +5,7 @@ class Solution {
     int left =0;
     int max_water=0;
     while(left<right){
-        int high=Math.min(height[left],height[right]);
-        int distance=right-left;
-        int water=high*distance;
+        int water=Math.min(height[left],height[right])*(right-left);
         max_water=Math.max(max_water,water);
         if(height[left]<height[right]){
             left++;
